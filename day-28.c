412833d8b74bbdfb534fcbd7139e52af5ee8fabb
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-14 21:24:31 +0600

    Update day-28.c

--- day-28.c
+++ day-28.c
@@ -16,5 +16,27 @@ if(i%2==0){
 }
 
 
+
+
+
+
+
+    // sum of digits practice
+
+  int n,sum=0,remin;
+scanf("%d",&n);
+
+//for(int i = 1;i<n;i++){
+while(n!=0){
+remin = n%10;
+sum = sum + remin;
+n = n/10;
+
+}
+
+printf("Sum of digits is %d",sum);
+//}
+
     return 0;
+
 }
