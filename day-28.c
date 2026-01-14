9d9544440270d9f45e5abe7c11587ecba38c63df
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-14 23:50:45 +0600

    Update day-28.c

--- day-28.c
+++ day-28.c
@@ -57,10 +57,29 @@ else{
 
 
 
+
+
+// Leap Year Checker
+
+int a;
+scanf("%d",&a);
+
+if(a%4==0){
+  printf("Leap year");
+}
+else {
+  printf("not a leap year");
+}
+
+
+
+
+    
     
 
     return 0;
 
 }
+
 
 
